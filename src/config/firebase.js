2e3b51099cbda0c23627d36ecad5f9dd175117a9
yosefs283 <yosefs283@gmail.com>
2025-03-01 21:29:54 +0200

    Remove sensitive files and update gitignore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,15 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBiqDoUajhxDxEG4o-SmmHWXSyQ6VSqVf8",
-    authDomain: "weight-tracker-b2774.firebaseapp.com",
-    projectId: "weight-tracker-b2774",
-    storageBucket: "weight-tracker-b2774.firebasestorage.app",
-    messagingSenderId: "636759293394",
-    appId: "1:636759293394:web:b1a73de0c40fd72b585c0b",
-    measurementId: "G-JRRZD3NKE1"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app); 
+export const db = getFirestore(app);
+export default app; 
